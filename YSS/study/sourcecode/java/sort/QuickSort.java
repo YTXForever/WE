0ee6d45a618e80a9e4f7bbf1d3de5dcb6c7ef680
yuhao <yuhao@daojia-inc.com>
2019-06-01 08:58:33 +0800

    sort

---
+++ YSS/study/sourcecode/java/sort/QuickSort.java
@@ -0,0 +1,25 @@
+package sort;
+
+import java.util.Arrays;
+
+/**
+ * 插入排序O(n2)
+ * 稳定
+ *
+ * @author yuh
+ * @date 2019-06-01 07:23
+ **/
+public class QuickSort {
+
+    public static void sort(int[] arr) {
+
+    }
+
+
+
+    public static void main(String[] args) {
+        int[] arr = {6, 5, 4, 3, 2, 3};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
