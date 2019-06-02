eb76fbda7351adf2f8c8d87cec1d4752e8baf73e
yuhao <yuhao@daojia-inc.com>
2019-06-02 19:04:37 +0800

    trie

--- YSS/study/sourcecode/java/socket/ServerSocketDemo.java
+++ YSS/study/sourcecode/java/socket/ServerSocketDemo.java
@@ -7,6 +7,7 @@ import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
+import java.util.concurrent.locks.LockSupport;
 
 /**
  * @author yuh
@@ -18,20 +19,21 @@ public class ServerSocketDemo {
         ServerSocket ss = new ServerSocket(8888);
         Socket accept = ss.accept();
         accept.setTcpNoDelay(true);
-        OutputStream outputStream = accept.getOutputStream();
-        InputStream inputStream = accept.getInputStream();
-        while (true) {
-            byte[] arr = new byte[128];
-            int read = inputStream.read(arr);
-            String s = new String(arr, 0, read);
-            System.out.println("from client:" +s);
-            if ("end".equals(s)) {
-                break;
-            }
-            outputStream.write(String.valueOf(s.length()).getBytes());
-            outputStream.flush();
-
-        }
-        accept.close();
+//        OutputStream outputStream = accept.getOutputStream();
+//        InputStream inputStream = accept.getInputStream();
+//        while (true) {
+//            byte[] arr = new byte[128];
+//            int read = inputStream.read(arr);
+//            String s = new String(arr, 0, read);
+//            System.out.println("from client:" +s);
+//            if ("end".equals(s)) {
+//                break;
+//            }
+//            outputStream.write(String.valueOf(s.length()).getBytes());
+//            outputStream.flush();
+//
+//        }
+//        accept.close();
+        LockSupport.park();
     }
 }
