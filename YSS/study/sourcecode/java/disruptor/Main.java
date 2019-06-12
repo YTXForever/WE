3b48d0f133f712fd18a3bd45b8421ae2f1a5daca
yuhao <yuhao@daojia-inc.com>
2019-06-12 10:29:18 +0800

    disruptor

---
+++ YSS/study/sourcecode/java/disruptor/Main.java
@@ -0,0 +1,45 @@
+package disruptor;
+
+import com.lmax.disruptor.BlockingWaitStrategy;
+import com.lmax.disruptor.EventHandler;
+import com.lmax.disruptor.RingBuffer;
+import com.lmax.disruptor.dsl.Disruptor;
+import com.lmax.disruptor.dsl.ProducerType;
+import io.netty.util.concurrent.DefaultThreadFactory;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
+/**
+ * @author yuh
+ * @date 2019-06-12 09:50
+ **/
+public class Main {
+
+    public static void main(String[] args) {
+        Disruptor<OrderEvent> disruptor = new Disruptor(
+                new OrderEventFactory(),
+                1024 * 1024,
+                Executors.defaultThreadFactory(),
+                ProducerType.SINGLE,
+                new BlockingWaitStrategy());
+
+        RingBuffer<OrderEvent> ringBuffer =
+                disruptor.getRingBuffer();
+
+        disruptor.handleEventsWith(new EventHandler<OrderEvent>() {
+            @Override
+            public void onEvent(OrderEvent event, long sequence, boolean endOfBatch) throws Exception {
+                System.out.println("sequence:" + sequence + ",orderId:" + event.getId());
+            }
+        });
+        disruptor.start();
+        for (int i = 0; i < 1000; i++) {
+            long next = ringBuffer.next();
+            OrderEvent orderEvent = ringBuffer.get(next);
+            orderEvent.setId(i);
+            ringBuffer.publish(next);
+        }
+    }
+
+}
--- YSS/study/sourcecode/java/recursion/Fibonacci.java
+++ YSS/study/sourcecode/java/recursion/Fibonacci.java
@@ -1,25 +1,41 @@
 package recursion;
 
 /**
+ * 尾递归都可以用循环优化 只要能列举的
  * @author yuh
  * @date 2019-05-31 08:12
  **/
 public class Fibonacci {
 
-    public static int fibonacci(int a){
-        if(a == 0){
+    public static int fibonacci(int a) {
+        if (a == 0) {
             return 0;
         }
-        if(a == 1){
+        if (a == 1) {
             return 1;
         }
-        return fibonacci(a-1) + fibonacci(a-2);
+        return fibonacci(a - 1) + fibonacci(a - 2);
+    }
+
+    public static int fibonacci1(int a) {
+        if (a == 0) {
+            return 0;
+        }
+        if (a == 1) {
+            return 1;
+        }
+        int i = 0, j = 1;
+        for (int k = 2; k <= a; k++) {
+            j += i;
+            i = j-i;
+        }
+        return j;
     }
 
 
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            System.out.println(fibonacci(i));
+            System.out.println(fibonacci(i)==fibonacci1(i));
         }
     }
 }
